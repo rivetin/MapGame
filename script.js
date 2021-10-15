84db3da9872b7c7d8c143508aae1ad8fff127475
rivetin <athulofficial01@gmail.com>
2021-10-15 10:03:24 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 10.525187980987075, lng: 76.2137848891439 },
+        zoom: 15,
+        mapId: 'd821e4808a5eb783',
+        disableDefaultUI: true
+    });
+}
+//10.525187980987075, 76.2137848891439
